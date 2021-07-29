77d60a1369697136c0b00b2243aaf945483175fd
NectarSpot <nectarspot@gmail.com>
2021-07-29 23:38:32 +0530

    11

--- src/components/support/index.js
+++ src/components/support/index.js
@@ -10,24 +10,24 @@ export function Support(){
           <div className={supportwrapper}>
 
             <div className={border}>
-                <h5 className={title}>
+                <p className={title}>
                 <span><BiSupport fill="#29abe2" size="24px" /></span>
                 SUPPORT
-                </h5>
+                </p>
                 <p className={subtitle}>hello@valuepetmeds.com</p>
             </div>
             <div className={border}>
-                <h5 className={title}>
+                <p className={title}>
                 <span><AiOutlineCar fill="#29abe2" size="24px" /></span>
                 FREE SHIPPING
-                </h5>
+                </p>
                 <p className={subtitle}>On orders over $49 - exclusions apply</p>
             </div>
             <div>
-                <h5 className={title}>
+                <p className={title}>
                 <span><HiOutlineRefresh stroke="#29abe2" size="24px" /></span>
                 MONEY BACK
-                </h5>
+                </p>
                 <p className={subtitle}>100% Money-back Guarantee</p>
             </div>
         
