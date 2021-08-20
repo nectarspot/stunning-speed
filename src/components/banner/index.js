09be1c62fe47569e6d2b9b97756caf72a2a008ca
NectarSpot <nectarspot@gmail.com>
2021-08-20 18:36:11 +0530

    mobilebanner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { bannerwrapper ,mainTitle , mobilehide} from "./banner.module.css"
+import { bannerwrapper ,mainTitle ,mobileShow} from "./banner.module.css"
 import { ShopButton } from "../shop-button"
 import { StaticImage } from "gatsby-plugin-image"
 
 export function Banner() {
 
     return( 
+       <div>
        <div className={bannerwrapper}>
-            <div className={mobilehide}>
+            
+            <div>
                 <StaticImage
                 src="https://cdn.shopify.com/s/files/1/0075/8995/3587/files/dog_1920x810_f590bc91-d6a3-45b3-9be7-01b21a9178bb.jpg?v=1626174111"
                 alt="value pet meds" />
@@ -20,5 +22,13 @@ export function Banner() {
                 SHOP
                 </ShopButton>
             </div>
+           
+        </div>
+
+        <div className={mobileShow}>
+        <StaticImage
+                src="https://cdn.shopify.com/s/files/1/0075/8995/3587/files/mobile-banner-updated_5f10d70a-f893-4e3c-899f-b753da601893.jpg?v=1556555594"
+                alt="value pet meds" />
+        </div>
         </div>
     ) }
